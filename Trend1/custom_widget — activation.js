402c89aeb23d7991ba74a0b3c7b76b6493d43db1
Roberto Chavez <rchavez.intertec@gmail.com>
2020-05-14 21:26:32 +0300

    Latest Update of the Trend with Libraries

---
+++ Trend1/custom_widget — activation.js
@@ -0,0 +1,12 @@
+﻿// A fucntion to run any cmd command
+//function runApp() {
+    // Using Windows Script Host Shell ActiveX to run cmd commands
+  //  WshShell = new ActiveXObject("WScript.Shell");
+    //WshShell.Run("D:\\Roberto\\SixthSemester\\Project\\BackEndRG\\backend\\bootstrap.bat");
+}
+//runApp();
+
+//var wshShell = new ActiveXObject("WScript.Shell");
+//wshShell.Run("D:\\dir\\user.bat");
+
+document.location.href="D:\\Roberto\\SixthSemester\\Project\\BackEndRG\\backend\\bootstrap.bat";
---
+++ backend/bootstrap.bat
@@ -0,0 +1,2 @@
+rem start chrome "file:///D:/Roberto/SixthSemester/Project/BackEndRG/Trend1/index.html"
+pm2 start D:\Roberto\SixthSemester\Project\BackEndRG\backend\index.js
