9339c39eb61a2c79c840f4290259c40c36564436
Roberto Chavez <rchavez.intertec@gmail.com>
2020-06-22 22:57:53 +0300

    the lastest commit will be this one

--- backend/data.js
+++ backend/data.js
@@ -4,8 +4,7 @@ Date.prototype.getUnixTime = function() { return this.getTime() };
 if(!Date.now) Date.now = function() { return new Date(); }
 Date.time = function() { return Date.now().getUnixTime(); }
 
-module.exports = async () => {
-    let config = {
+let config = {
         user: "sa",
         password: "2905904919301",
         server: "DESKTOP-H54R1TJ\\SQLEXPRESS",
@@ -13,34 +12,58 @@ module.exports = async () => {
         port: 1433
     };
 
-    try {
-        // make sure that any items are correctly URL encoded in the connection string
-		
-        await sql.connect(config);
-        var result = await sql.query`SELECT TagTimeStamp, Value FROM SEICEVENTHISTORY ORDER BY TagTimeStamp ASC`;
-	//console.log(result);	
-	var myKeyIdentifier = Object.keys(result)[1]; // ignore this line bro
-	console.log(myKeyIdentifier); // also ignore this line
-	var lres = result["recordset"];	// here I am getting the records that I need with the proper key
-	var myData = lres.map(function(item) { // with this function I am adjusting the data to create the chart
-			return {x : item.TagTimeStamp, y : item.Value };
-		});
-	var mySecondData = lres.map(function(item) { // with this function I am adjusting the data to create the chart
-			 return [new Date(item.TagTimeStamp).getUnixTime(), item.Value];
-			  
-		});
-		
-	//console.log(lres);
-	console.log("This is the data to use for the Chart");
-	//console.log(myData);
-        var suka = [];
-        suka = [{"x": new Date("2019-10-04T22:00:00.000Z"),"y":5},{"x": new Date("2019-10-04T22:00:00.830Z"),"y":6}];
-        console.log(mySecondData[0][0])
+const getData = async () => {    
+  try {          
+  //here we divide the code of mssql//////////////////////////////  
+  
+  await sql.connect(config);        
+  var result = await sql.query`SELECT TagTimeStamp, Value FROM SEICEVENTHISTORY ORDER BY TagTimeStamp ASC`;
+  
+  
+  var lres = result["recordset"];  // here I am getting the records that I need with the proper key
+  
+  var mySecondData = lres.map(function(item) { // with this function I am adjusting the data to create the chart
+       return [new Date(item.TagTimeStamp).getUnixTime(), item.Value];
+        
+    });     
+        
         return mySecondData;
-	//return mySecondData[0];
+  //return [mySecondData[0], mySecondData[1]]; //////////////////////////////
     } catch (err) {
         console.log(err);
         return { hello: "world" };
     }
 };
 
+//const fromDate = ...
+  //const toDate = ....  
+const getDataWithLimits = async (fromDate, toDate) => {
+
+try {    
+     //here we divide the code of mssql//////////////////////////////  
+        await sql.connect(config);
+        var result = await sql.query(`SELECT TagTimeStamp, Value FROM SEICEVENTHISTORY WHERE TagTimeStamp BETWEEN '${fromDate}' AND '${toDate}' ORDER BY TagTimeStamp ASC;`);
+  
+   
+  //var myKeyIdentifier = Object.keys(result)[1]; // ignore this line bro
+  
+  var lres = result["recordset"];  // here I am getting the records that I need with the proper key
+  //var myData = lres.map(function(item) { // with this function I am adjusting the data to create the chart
+    //  return {x : item.TagTimeStamp, y : item.Value };
+    //});
+  var mySecondData = lres.map(function(item) { // with this function I am adjusting the data to create the chart
+       return [new Date(item.TagTimeStamp).getUnixTime(), item.Value];
+        
+    });    
+ 
+        
+        return mySecondData;
+  //return [mySecondData[0], mySecondData[1]]; //////////////////////////////
+    } catch (err) {
+        console.log(err);
+        return { hello: "world" };
+    }
+
+};
+
+module.exports = {getData, getDataWithLimits};
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const getData = require("./data");
+//const getData = require("./data");
+const  {getData, getDataWithLimits} = require('./data');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+
+//var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", async (req, res) => {
   const data = await getData();
   return res.send(data);
 });
 
+//Here the other server to receive the data from the frontend
+app.post('/:from-:to', async (req, res) => {
+  const {timestamp, timestamp2} = req.body;
+  const todin = new Date(timestamp).toISOString()
+  const tdva = new Date(timestamp2).toISOString()
+  console.log(todin, tdva);   
+  const data = await getDataWithLimits(timestamp, timestamp2);
+  return res.send(data);
+});
+
 app.listen(5000, () => console.log("listening on port 5000"));
 
 module.exports = app;
+
+
